01b489b952f148832f4c54219d56ddb15867494c
Arthur Vieira <arthur.gamer20@gmail.com>
2021-08-17 20:13:19 -0300

    Aula 17/08/2021 - salvar dados

--- src/main/java/br/com/senac/controller/AlunoController.java
+++ src/main/java/br/com/senac/controller/AlunoController.java
@@ -3,9 +3,11 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.domain.Aluno;
 import br.com.senac.service.AlunoService;
 
 @Controller
@@ -17,7 +19,7 @@ public class AlunoController {
 	@Autowired
 	private AlunoService alunoService;
 	
-	@GetMapping("/listarAlunos")
+	@GetMapping("/listar")
 	public ModelAndView listaTodosAlunos() {
 		//Vai pro html
 		ModelAndView mv = new ModelAndView("aluno/listarAluno");
@@ -25,4 +27,19 @@ public class AlunoController {
 		return mv;
 	}
 	
+	
+	//criar rota para chamar a tela de cadastro
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarAluno() {
+		ModelAndView mv = new ModelAndView("aluno/cadastrarAluno");
+		mv.addObject("alunoFaculdade", new Aluno());
+		return mv;
+	}
+	
+	//criar rota para incluir o aluno
+	@PostMapping("/salvar")
+	public ModelAndView salvarAluno(Aluno aluno) {
+		alunoService.salvar(aluno);
+		return listaTodosAlunos();
+	}
 }
--- src/main/java/br/com/senac/controller/ProfessorController.java
+++ src/main/java/br/com/senac/controller/ProfessorController.java
@@ -3,10 +3,11 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import br.com.senac.domain.Professor;
 import br.com.senac.service.ProfessorService;
 
 @Controller
@@ -18,12 +19,26 @@ public class ProfessorController {
 	@Autowired
 	private ProfessorService profService;
 	
-	@GetMapping("/listarProf")
-	public ModelAndView listaTodosPrefessores() {
+	@GetMapping("/listar")
+	public ModelAndView listaTodosProfessores() {
 		//Vai pro html
 		ModelAndView mv = new ModelAndView("prof/listarProf");
 		mv.addObject("professores",profService.buscarTodosProfessores());
 		return mv;
+	}
+	
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarProfessor() {
+		ModelAndView mv = new ModelAndView("prof/cadastrarProfessor");
+		mv.addObject("profFaculdade", new Professor());
+		return mv;
+	}
+	
+	//criar rota para incluir o aluno
+	@PostMapping("/salvar")
+	public ModelAndView salvarProfessor(Professor professor) {
+		profService.salvar(professor);
+		return listaTodosProfessores();
 	}
 	
 }
